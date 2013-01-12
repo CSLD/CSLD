54a6a88718a29c2aaeb6365e08870237c566699f
Jakub Balhar <balhar.jakub@gmail.com>
2013-01-12 09:17:33 +0100

    Připojení k DB místo v Session při každé načtené stránce.

---
+++ sql/delta/20130104.sql
@@ -0,0 +1,13 @@
+insert into resource (id, path, default_path) values (DEFAULT, '/editaceObrazku', '/');
+
+insert into role_has_resource (role_id, resource_id) values (2,29);
+insert into role_has_resource (role_id, resource_id) values (3,29);
+insert into role_has_resource (role_id, resource_id) values (4,29);
+insert into role_has_resource (role_id, resource_id) values (5,29);
+
+insert into resource (id, path, default_path) values (DEFAULT, '/handlers/editaceObrazkuHry', '/');
+
+insert into role_has_resource (role_id, resource_id) values (2,30);
+insert into role_has_resource (role_id, resource_id) values (3,30);
+insert into role_has_resource (role_id, resource_id) values (4,30);
+insert into role_has_resource (role_id, resource_id) values (5,30);
