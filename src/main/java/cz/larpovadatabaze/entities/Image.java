86343135a49b5a49f8662f22ce9b4b5b0e36665a
Jakub Balhar <balhar.jakub@gmail.com>
2013-07-06 12:20:59 +0200

    Working no image

--- src/main/java/cz/larpovadatabaze/entities/Image.java
+++ src/main/java/cz/larpovadatabaze/entities/Image.java
@@ -32,7 +32,7 @@ public class Image implements Serializable {
     @Column(name = "path", nullable = false, insertable = true, updatable = true, length = 2147483647, precision = 0)
     @Basic
     public String getPath() {
-        return path != null ? path : null;
+        return path != null ? path : "files/img/author_icon.png";
     }
 
     public void setPath(String path) {
