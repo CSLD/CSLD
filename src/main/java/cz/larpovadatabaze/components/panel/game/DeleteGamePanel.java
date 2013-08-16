2d1d17ef5b3212c2f79bd5e57c0010e6e4b56e51
balhar <balhar.jakub@gmail.com>
2013-08-16 08:17:47 +0200

    Minor bug repairs

--- src/main/java/cz/larpovadatabaze/components/panel/game/DeleteGamePanel.java
+++ src/main/java/cz/larpovadatabaze/components/panel/game/DeleteGamePanel.java
@@ -2,11 +2,10 @@ package cz.larpovadatabaze.components.panel.game;
 
 import cz.larpovadatabaze.components.page.CsldBasePage;
 import cz.larpovadatabaze.components.page.game.DeleteGamePage;
-import cz.larpovadatabaze.components.page.group.CreateOrUpdateGroupPage;
+import cz.larpovadatabaze.entities.CsldUser;
 import cz.larpovadatabaze.entities.Game;
 import cz.larpovadatabaze.security.CsldAuthenticatedWebSession;
 import cz.larpovadatabaze.security.CsldRoles;
-import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -27,6 +26,13 @@ public class DeleteGamePanel extends Panel {
 
     @Override
     protected void onConfigure() {
-        setVisibilityAllowed(((CsldAuthenticatedWebSession) CsldAuthenticatedWebSession.get()).getLoggedUser().getRole() > CsldRoles.USER.getRole());
+        CsldUser logged = ((CsldAuthenticatedWebSession) CsldAuthenticatedWebSession.get()).getLoggedUser();
+        boolean visible = false;
+        if(logged != null){
+            if(logged.getRole() > CsldRoles.USER.getRole()) {
+                visible = true;
+            }
+        }
+        setVisibilityAllowed(visible);
     }
 }
