9bd83140eb42f214c33ddd3a3d8c30fba1c248eb
Michal Kára <lemming@ucw.cz>
2013-11-05 20:55:43 +0100

    FAQ graphics fixed

--- src/main/java/cz/larpovadatabaze/components/panel/home/LastCommentsPanel.java
+++ src/main/java/cz/larpovadatabaze/components/panel/home/LastCommentsPanel.java
@@ -15,6 +15,7 @@ import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
@@ -31,75 +32,86 @@ import java.util.List;
 public class LastCommentsPanel extends Panel {
     @SpringBean
     CommentService commentService;
-    private final int MAX_CHARS_IN_COMMENT = 80;
+    private static final int MAX_CHARS_IN_COMMENT = 160;
+    private static int INITIAL_LAST_COMMENTS = 3;
+    private static int EXPANDED_LAST_COMMENTS = 10;
+
+    private class CommentsView extends ListView<Comment> {
+        public CommentsView(String id, List<? extends Comment> list) {
+            super(id, list);
+        }
+
+        @Override
+        protected void populateItem(ListItem<Comment> item) {
+            Comment comment = item.getModelObject();
+            Game game = comment.getGame();
+            CsldUser commenter = comment.getUser();
+
+            Fragment commentFragment = new Fragment("comment", "commentFragment", LastCommentsPanel.this);
+            item.add(commentFragment);
+
+            PageParameters gameParams = new PageParameters();
+            gameParams.add("id", game.getId());
+            PageParameters userParams = new PageParameters();
+            userParams.add("id", commenter.getId());
+
+            if(commenter.getImage() == null){
+                commenter.setImage(cz.larpovadatabaze.entities.Image.getDefaultUser());
+            }
+
+            final BookmarkablePageLink<CsldBasePage> commenterIconLink =
+                    new BookmarkablePageLink<CsldBasePage>("commenterIconLink", UserDetail.class, userParams);
+            final Image commenterIcon = new Image("commenterIcon",
+                    new ContextRelativeResource(commenter.getImage().getPath()));
+            commenterIconLink.add(commenterIcon);
+            commentFragment.add(commenterIconLink);
+
+            final BookmarkablePageLink<CsldBasePage> commenterLink =
+                    new BookmarkablePageLink<CsldBasePage>("commenterLink", UserDetail.class, userParams);
+            Label commenterName = new Label("commenterName",
+                    Model.of(commenter.getPerson().getNickname() + " - " +  commenter.getPerson().getName()));
+            commenterLink.add(commenterName);
+            commentFragment.add(commenterLink);
+
+            SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy");
+            Date dateOfComment = new Date();
+            dateOfComment.setTime(comment.getAdded().getTime());
+
+            Label commentDate = new Label("commentDate", Model.of(formatDate.format(dateOfComment)));
+            commentFragment.add(commentDate);
+
+            String gameRatingColor = Rating.getColorOf(game.getTotalRating());
+            Label gameRating = new Label("gameRating","");
+            gameRating.add(new AttributeAppender("class", Model.of(gameRatingColor), " "));
+            commentFragment.add(gameRating);
+
+            final BookmarkablePageLink<CsldBasePage> gameLinkContent =
+                    new BookmarkablePageLink<CsldBasePage>("gameLink", GameDetail.class, gameParams);
+            final Label gameName = new Label("gameName", game.getName());
+            gameLinkContent.add(gameName);
+            commentFragment.add(gameLinkContent);
+
+            String commentToShow = comment.getComment();
+            if(commentToShow.length() > MAX_CHARS_IN_COMMENT){
+                commentToShow = commentToShow.substring(0,MAX_CHARS_IN_COMMENT);
+            }
+            commentFragment.add(new Label("commentsContent", Model.of(commentToShow)).setEscapeModelStrings(false));
+            final BookmarkablePageLink<CsldBasePage> gameMoreLink =
+                    new BookmarkablePageLink<CsldBasePage>("gameMoreLink", GameDetail.class, gameParams);
+            commentFragment.add(gameMoreLink);
+        }
+    }
 
     public LastCommentsPanel(String id) {
         super(id);
 
-        int AMOUNT_LAST_COMMENTS = 3;
-        List<Comment> toShow = commentService.getLastComments(AMOUNT_LAST_COMMENTS);
-        ListView<Comment> gamesView = new ListView<Comment>("commentsView", toShow) {
-            @Override
-            protected void populateItem(ListItem<Comment> item) {
-                Comment comment = item.getModelObject();
-                Game game = comment.getGame();
-                CsldUser commenter = comment.getUser();
+        List<Comment> toShow = commentService.getLastComments(EXPANDED_LAST_COMMENTS);
 
-                PageParameters gameParams = new PageParameters();
-                gameParams.add("id", game.getId());
-                PageParameters userParams = new PageParameters();
-                userParams.add("id", commenter.getId());
+        add(new CommentsView("visibleComments", toShow.subList(0, INITIAL_LAST_COMMENTS)));
+        add(new CommentsView("hiddenComments", toShow.subList(INITIAL_LAST_COMMENTS, toShow.size())));
 
-                if(commenter.getImage() == null){
-                    commenter.setImage(cz.larpovadatabaze.entities.Image.getDefaultUser());
-                }
-
-                final BookmarkablePageLink<CsldBasePage> commenterIconLink =
-                        new BookmarkablePageLink<CsldBasePage>("commenterIconLink", UserDetail.class, userParams);
-                final Image commenterIcon = new Image("commenterIcon",
-                        new ContextRelativeResource(commenter.getImage().getPath()));
-                commenterIconLink.add(commenterIcon);
-                item.add(commenterIconLink);
-
-                final BookmarkablePageLink<CsldBasePage> commenterLink =
-                        new BookmarkablePageLink<CsldBasePage>("commenterLink", UserDetail.class, userParams);
-                Label commenterName = new Label("commenterName",
-                        Model.of(commenter.getPerson().getNickname() + " - " +  commenter.getPerson().getName()));
-                commenterLink.add(commenterName);
-                item.add(commenterLink);
-
-                SimpleDateFormat formatDate = new SimpleDateFormat("dd.MM.yyyy");
-                Date dateOfComment = new Date();
-                dateOfComment.setTime(comment.getAdded().getTime());
-
-                Label commentDate = new Label("commentDate", Model.of(formatDate.format(dateOfComment)));
-                item.add(commentDate);
-
-                String gameRatingColor = Rating.getColorOf(game.getTotalRating());
-                Label gameRating = new Label("gameRating","");
-                gameRating.add(new AttributeAppender("class", Model.of(gameRatingColor), " "));
-                item.add(gameRating);
-
-                final BookmarkablePageLink<CsldBasePage> gameLinkContent =
-                        new BookmarkablePageLink<CsldBasePage>("gameLink", GameDetail.class, gameParams);
-                final Label gameName = new Label("gameName", game.getName());
-                gameLinkContent.add(gameName);
-                item.add(gameLinkContent);
-
-                String commentToShow = comment.getComment();
-                if(commentToShow.length() > MAX_CHARS_IN_COMMENT){
-                    commentToShow = commentToShow.substring(0,MAX_CHARS_IN_COMMENT);
-                }
-                item.add(new Label("commentsContent", Model.of(commentToShow)).setEscapeModelStrings(false));
-                final BookmarkablePageLink<CsldBasePage> gameMoreLink =
-                        new BookmarkablePageLink<CsldBasePage>("gameMoreLink", GameDetail.class, gameParams);
-                item.add(gameMoreLink);
-            }
-        };
-        add(gamesView);
-
-        final BookmarkablePageLink<CsldBasePage> moreComments =
-                new BookmarkablePageLink<CsldBasePage>("moreComments", ListComments.class);
-        add(moreComments);
+        final BookmarkablePageLink<CsldBasePage> allComments =
+                new BookmarkablePageLink<CsldBasePage>("allComments", ListComments.class);
+        add(allComments);
     }
 }
--- src/main/java/cz/larpovadatabaze/components/panel/home/LastGamesPanel.java
+++ src/main/java/cz/larpovadatabaze/components/panel/home/LastGamesPanel.java
@@ -12,7 +12,9 @@ import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.request.resource.ContextRelativeResource;
@@ -26,61 +28,81 @@ import java.util.List;
 public class LastGamesPanel extends Panel {
     @SpringBean
     GameService gameService;
-    private final int MAX_CHARS = 80;
+    private final static int MAX_CHARS = 160;
+    private final static int INITIAL_AMOUNT_LAST_GAMES = 3;
+    private final static int EXPANDED_AMOUNT_LAST_GAMES = 10;
+
+    private IModel<List<Game>> gamesModel;
+
+    /**
+     * List view of games
+     */
+    private class GamesListView extends ListView<Game> {
+        public GamesListView(String id, List<? extends Game> list) {
+            super(id, list);
+        }
+
+        @Override
+        protected void populateItem(ListItem<Game> item) {
+            Game game = item.getModelObject();
+
+            Fragment gameFragment = new Fragment("game", "gameFragment", LastGamesPanel.this);
+            item.add(gameFragment);
+
+            PageParameters params = new PageParameters();
+            params.add("id", game.getId());
+
+            if(game.getImage() == null){
+                game.setImage(cz.larpovadatabaze.entities.Image.getDefaultGame());
+            }
+
+            final BookmarkablePageLink<CsldBasePage> gameLink =
+                    new BookmarkablePageLink<CsldBasePage>("gameIconLink", GameDetail.class, params);
+            final Image gameLinkImage = new Image("gameIcon",
+                    new ContextRelativeResource(game.getImage().getPath()));
+            gameLink.add(gameLinkImage);
+            gameFragment.add(gameLink);
+
+            String gameRatingColor = Rating.getColorOf(game.getTotalRating());
+            Label gameRating = new Label("gameRating","");
+            gameRating.add(new AttributeAppender("class", Model.of(gameRatingColor), " "));
+            gameFragment.add(gameRating);
+
+            final BookmarkablePageLink<CsldBasePage> gameLinkContent =
+                    new BookmarkablePageLink<CsldBasePage>("gameLink", GameDetail.class, params);
+            final Label gameName = new Label("gameName", game.getName());
+            gameLinkContent.add(gameName);
+            gameFragment.add(gameLinkContent);
+
+            gameFragment.add(new Label("players", Model.of(game.getPlayers())));
+            gameFragment.add(new Label("gameDescription",
+                    Model.of(
+                            game.getDescription().length() > MAX_CHARS ?
+                                    game.getDescription().substring(0,MAX_CHARS) :
+                                    game.getDescription()))
+            );
+            final BookmarkablePageLink<CsldBasePage> gameMoreLink =
+                    new BookmarkablePageLink<CsldBasePage>("gameMoreLink", GameDetail.class, params);
+            gameFragment.add(gameMoreLink);
+        }
+    }
 
     public LastGamesPanel(String id) {
         super(id);
+    }
 
-        int AMOUNT_LAST_GAMES = 3;
-        List<Game> toShow = gameService.getLastGames(AMOUNT_LAST_GAMES);
+    @Override
+    public void onInitialize() {
+        super.onInitialize();;
 
+        List<Game> toShow = gameService.getLastGames(EXPANDED_AMOUNT_LAST_GAMES);
 
-        ListView<Game> gamesView = new ListView<Game>("gamesView", toShow) {
-            @Override
-            protected void populateItem(ListItem<Game> item) {
-                Game game = item.getModelObject();
+        // XXX - this will throw exception if we ever have toofew games - XXX
+        add(new GamesListView("visibleGamesView", toShow.subList(0, INITIAL_AMOUNT_LAST_GAMES)));
+        add(new GamesListView("hiddenGamesView", toShow.subList(INITIAL_AMOUNT_LAST_GAMES, toShow.size())));
 
-                PageParameters params = new PageParameters();
-                params.add("id", game.getId());
-
-                if(game.getImage() == null){
-                    game.setImage(cz.larpovadatabaze.entities.Image.getDefaultGame());
-                }
-
-                final BookmarkablePageLink<CsldBasePage> gameLink =
-                        new BookmarkablePageLink<CsldBasePage>("gameIconLink", GameDetail.class, params);
-                final Image gameLinkImage = new Image("gameIcon",
-                        new ContextRelativeResource(game.getImage().getPath()));
-                gameLink.add(gameLinkImage);
-                item.add(gameLink);
-
-                String gameRatingColor = Rating.getColorOf(game.getTotalRating());
-                Label gameRating = new Label("gameRating","");
-                gameRating.add(new AttributeAppender("class", Model.of(gameRatingColor), " "));
-                item.add(gameRating);
-
-                final BookmarkablePageLink<CsldBasePage> gameLinkContent =
-                        new BookmarkablePageLink<CsldBasePage>("gameLink", GameDetail.class, params);
-                final Label gameName = new Label("gameName", game.getName());
-                gameLinkContent.add(gameName);
-                item.add(gameLinkContent);
-
-                item.add(new Label("players", Model.of(game.getPlayers())));
-                item.add(new Label("gameDescription",
-                        Model.of(
-                                game.getDescription().length() > MAX_CHARS ?
-                                        game.getDescription().substring(0,MAX_CHARS) :
-                                        game.getDescription()))
-                );
-                final BookmarkablePageLink<CsldBasePage> gameMoreLink =
-                        new BookmarkablePageLink<CsldBasePage>("gameMoreLink", GameDetail.class, params);
-                item.add(gameMoreLink);
-            }
-        };
-        add(gamesView);
-
-        final BookmarkablePageLink<CsldBasePage> moreGames =
-                new BookmarkablePageLink<CsldBasePage>("moreGames", ListLastGames.class);
-        add(moreGames);
+        final BookmarkablePageLink<CsldBasePage> allGames =
+                new BookmarkablePageLink<CsldBasePage>("allGames", ListLastGames.class);
+        add(allGames);
     }
 }
