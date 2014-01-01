516b6a94c608599f7b6e0a5eb036e0e5ca8743aa
Michal Kára <lemming@ucw.cz>
2014-01-01 12:56:39 +0100

    Dodělány popisky ke štítkům a štítky drobet lépe graficky zpracovány

--- src/main/java/cz/larpovadatabaze/components/panel/game/GameDetailPanel.java
+++ src/main/java/cz/larpovadatabaze/components/panel/game/GameDetailPanel.java
@@ -7,6 +7,7 @@ import cz.larpovadatabaze.entities.CsldGroup;
 import cz.larpovadatabaze.entities.CsldUser;
 import cz.larpovadatabaze.entities.Game;
 import cz.larpovadatabaze.services.ImageService;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
@@ -50,7 +51,9 @@ public class GameDetailPanel extends Panel {
             @Override
             protected void populateItem(ListItem<cz.larpovadatabaze.entities.Label> item) {
                 cz.larpovadatabaze.entities.Label label = item.getModelObject();
-                item.add(new Label("label", label.getName()));
+                Label labelC = new Label("label", label.getName());
+                labelC.add(new AttributeModifier("title", label.getDescription()));
+                item.add(labelC);
             }
         };
         add(view);
