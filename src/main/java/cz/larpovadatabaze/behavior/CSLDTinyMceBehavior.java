75dbba95992246272a1ae06d9bb4fdf1054d551f
Michal Kára <lemming@ucw.cz>
2014-01-03 22:06:50 +0100

    Handling paste v tinyMCE

--- src/main/java/cz/larpovadatabaze/behavior/CSLDTinyMceBehavior.java
+++ src/main/java/cz/larpovadatabaze/behavior/CSLDTinyMceBehavior.java
@@ -17,6 +17,16 @@ public class CSLDTinyMceBehavior extends TinyMceBehavior {
         public CLSDTinyMceSettings() {
             super();
             setContentCss(new CssResourceReference(CSLDTinyMceBehavior.class, "tinymce_custom.css"));
+            addCustomSetting("plugins: 'paste'");
+            addCustomSetting("paste_text_sticky: true");
+            addCustomSetting("paste_text_sticky_default: true");
+            /* (if we ever want a bit formatting kept, remove "sticky" lines above and allow the ones below
+            addCustomSetting("paste_auto_cleanup_on_paste: true");
+            addCustomSetting("paste_remove_spans: true");
+            addCustomSetting("paste_remove_styles: true");
+            addCustomSetting("paste_remove_styles_if_webkit: true");
+            addCustomSetting("paste_strip_class_attributes: 'all'");
+            */
         }
     }
 
