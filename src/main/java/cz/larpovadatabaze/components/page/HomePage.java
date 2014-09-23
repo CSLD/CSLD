b202a8431c636cba3db010dcdb866e888c9bf8ce
Jakub Balhar <balhar.jakub@gmail.com>
2014-09-23 22:57:50 +0200

    Started implementing timeline.

--- src/main/java/cz/larpovadatabaze/components/page/HomePage.java
+++ src/main/java/cz/larpovadatabaze/components/page/HomePage.java
@@ -6,12 +6,37 @@ import cz.larpovadatabaze.components.panel.home.LastGamesPanel;
 import cz.larpovadatabaze.components.panel.home.RandomLarpPanel;
 import cz.larpovadatabaze.components.panel.home.StatisticsPanel;
 import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.model.IModel;
+import org.wicketstuff.simile.timeline.Timeline;
+import org.wicketstuff.simile.timeline.TimelineEventModel;
+import org.wicketstuff.simile.timeline.TimelineModel;
 
 /**
  *
  */
 public class HomePage extends CsldBasePage {
     public HomePage(){
+        IModel<TimelineModel> timelineModel = new IModel<TimelineModel>() {
+            TimelineModel timeline;
+
+            @Override
+            public TimelineModel getObject() {
+                return timeline;
+            }
+
+            @Override
+            public void setObject(TimelineModel object) {
+                this.timeline = object;
+            }
+
+            @Override
+            public void detach() {}
+        };
+        TimelineModel timeline = new TimelineModel();
+        // timeline.addEvent(new TimelineEventModel());
+        timelineModel.setObject(timeline);
+
+        add(new Timeline("timeline", timelineModel));
         add(new LastGamesPanel("lastGames"));
         add(new LastCommentsPanel("lastComments"));
 
