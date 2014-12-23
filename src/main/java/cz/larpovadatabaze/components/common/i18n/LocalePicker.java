26e8a3789201051c9ab17822d69b957283ed41b0
Kara Michal <Michal.Kara@airbank.cz>
2014-12-23 11:40:49 +0100

    Language selection via graphical flags

--- src/main/java/cz/larpovadatabaze/components/common/i18n/LocalePicker.java
+++ src/main/java/cz/larpovadatabaze/components/common/i18n/LocalePicker.java
@@ -1,16 +1,19 @@
 package cz.larpovadatabaze.components.common.i18n;
 
-import cz.larpovadatabaze.lang.CodeLocaleProvider;
 import org.apache.wicket.RestartResponseException;
 import org.apache.wicket.Session;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.DropDownChoice;
-import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.model.Model;
 
-import java.util.List;
 import java.util.Locale;
+
+import cz.larpovadatabaze.lang.CodeLocaleProvider;
 
 /**
  * Created by jbalhar on 11/30/2014.
@@ -21,26 +24,46 @@ public class LocalePicker extends Panel{
         super(id);
     }
 
+    private Image createLocaleFlag(Locale locale) {
+        return new Image("flag", locale.getLanguage()+".png");
+    }
+
+    private Label createLocaleLabel(Locale locale) {
+        return new Label("name", locale.getLanguage().toUpperCase());
+    }
+
     @Override
     protected void onInitialize() {
         super.onInitialize();
 
-        // Set default Locale based on actual Locale.
+        // Create view
+        RepeatingView locales = new RepeatingView("locales");
 
-        List<Locale> locales = new CodeLocaleProvider().availableLocale();
-        final DropDownChoice<Locale> changeLocale =
-                new DropDownChoice<Locale>("changeLocale", new Model<Locale>(Session.get().getLocale()), locales);
-        StatelessForm form = new StatelessForm("form"){
-            @Override
-            protected void onSubmit() {
-                Session.get().setLocale(changeLocale.getModelObject());
-                throw new RestartResponseException(getPage().getPageClass());
+        // Draw current locale
+        Fragment currentLocalePanel = new Fragment(locales.newChildId(), "localeItemSelected", this);
+        Locale currentLocale = Session.get().getLocale();
+        currentLocalePanel.add(createLocaleFlag(currentLocale));
+        currentLocalePanel.add(createLocaleLabel(currentLocale));
+        locales.add(currentLocalePanel);
+
+        // Draw other locales
+        for(Locale locale : new CodeLocaleProvider().availableLocale()) {
+            if (!locale.getLanguage().equals(currentLocale.getLanguage())) { // Do not add selected locale
+                Fragment localePanel = new Fragment(locales.newChildId(), "localeItemAvailable", this);
+                AjaxLink<Locale> link = new AjaxLink<Locale>("link", Model.of(locale)) {
+                    @Override
+                    public void onClick(AjaxRequestTarget ajaxRequestTarget) {
+                        Session.get().setLocale(getModelObject());
+                        throw new RestartResponseException(getPage().getPageClass());
+                    }
+                };
+                localePanel.add(link);
+                link.add(createLocaleFlag(locale));
+                link.add(createLocaleLabel(locale));
+                locales.add(localePanel);
             }
-        };
+        }
 
-        form.add(new Button("submit"));
-        form.add(changeLocale);
-
-        add(form);
+        add(locales);
     }
 }
