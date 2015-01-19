8acc7ae7534c62dd65c04e266598dfb49603c06f
Jakub Balhar <balhar.jakub@gmail.com>
2015-01-19 22:50:45 +0100

    CSLD-176 Fix issue when language in browser is represented differently then allowed ones.   English is default language when no other is available.

--- src/main/java/cz/larpovadatabaze/Csld.java
+++ src/main/java/cz/larpovadatabaze/Csld.java
@@ -31,6 +31,7 @@ import org.apache.log4j.Logger;
 import org.apache.wicket.ConverterLocator;
 import org.apache.wicket.IConverterLocator;
 import org.apache.wicket.RuntimeConfigurationType;
+import org.apache.wicket.Session;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.core.request.handler.BookmarkableListenerInterfaceRequestHandler;
@@ -40,6 +41,8 @@ import org.apache.wicket.markup.html.IPackageResourceGuard;
 import org.apache.wicket.markup.html.SecurePackageResourceGuard;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.IRequestHandler;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.request.component.IRequestablePage;
 import org.apache.wicket.request.cycle.AbstractRequestCycleListener;
@@ -52,6 +55,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -247,4 +253,15 @@ public class Csld extends AuthenticatedWebApplication implements ApplicationCont
 
     protected boolean isDevelopmentMode() {
         return RuntimeConfigurationType.DEVELOPMENT.equals(this.getConfigurationType());
-    }}
+    }
+
+    @Override
+    public Session newSession(Request request, Response response) {
+        Session session = super.newSession(request, response);
+        List<Locale> availableLocale = new CodeLocaleProvider().availableLocale();
+        if(!availableLocale.contains(session.getLocale())){
+            session.setLocale(Locale.ENGLISH);
+        }
+        return session;
+    }
+}
