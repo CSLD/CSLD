65240c5c7e365f3e545d11a1662c94681474ce56
Kara Michal <Michal.Kara@airbank.cz>
2015-07-19 13:33:34 +0200

    UserHasLanguages made serializable

--- src/main/java/cz/larpovadatabaze/entities/UserHasLanguages.java
+++ src/main/java/cz/larpovadatabaze/entities/UserHasLanguages.java
@@ -1,13 +1,24 @@
 package cz.larpovadatabaze.entities;
 
-import javax.persistence.*;
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 /**
  * Join table among user and languages they are interested in.
  */
 @Entity
 @Table(name="csld_user_has_languages", schema = "public")
-public class UserHasLanguages {
+public class UserHasLanguages implements Serializable {
     @Column(name = "id", nullable = false, insertable = true, updatable = true )
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_key_gen")
