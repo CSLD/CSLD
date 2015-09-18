191ddf38dc1e4aeb05831eb6b3d775bef1e3c014
Jakub Balhar <jakub@balhar.net>
2015-09-18 11:56:51 +0200

    #198 Last News removed from the HomePage

--- src/main/java/cz/larpovadatabaze/components/page/HomePage.java
+++ src/main/java/cz/larpovadatabaze/components/page/HomePage.java
@@ -40,8 +40,6 @@ public class HomePage extends CsldBasePage {
 
         add(new LastCommentsPanel("lastComments"));
 
-        add(new NewsListPanel("news"));
-
         add(new RecentPhotosPanel("recentPhotos"));
     }
 
