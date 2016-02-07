a8426e57236854f46a32ba53224bba95060659c2
Jakub Balhar <jakub@balhar.net>
2016-02-07 15:17:43 +0100

    Remove Session based Language from visible ones.

--- src/main/java/cz/larpovadatabaze/lang/DbSessionLanguageSolver.java
+++ src/main/java/cz/larpovadatabaze/lang/DbSessionLanguageSolver.java
@@ -5,40 +5,39 @@ import cz.larpovadatabaze.entities.UserHasLanguages;
 import cz.larpovadatabaze.utils.UserUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 /**
  * It gets languages from session as well as from information about logged user in the database.
  */
 @Service
 public class DbSessionLanguageSolver implements LanguageSolver {
-    private LanguageSolver solveLang;
-
-    public DbSessionLanguageSolver(){
-        solveLang = new SessionLanguageSolver();
-    }
+    public DbSessionLanguageSolver(){}
 
     @Override
     public List<Locale> getLanguagesForUser() {
-        List<Locale> allLocales = solveLang.getLanguagesForUser();
+        List<Locale> allLocales = new ArrayList<>();
         CsldUser loggedUser = UserUtils.getLoggedUser();
         if(loggedUser != null) {
-            for(UserHasLanguages lang: loggedUser.getUserHasLanguages()){
-                allLocales.add(Locale.forLanguageTag(lang.getLanguage()));
-            }
+            allLocales.addAll(loggedUser.getUserHasLanguages().stream()
+                    .map(lang -> Locale.forLanguageTag(lang.getLanguage()))
+                    .collect(Collectors.toList()));
         }
         return allLocales;
     }
 
     @Override
     public List<String> getTextLangForUser() {
-        List<String> allLocales = solveLang.getTextLangForUser();
+        List<String> allLocales = new ArrayList<>();
         CsldUser loggedUser = UserUtils.getLoggedUser();
         if(loggedUser != null) {
-            for(UserHasLanguages lang: loggedUser.getUserHasLanguages()){
-                allLocales.add(lang.getLanguage());
-            }
+            allLocales.addAll(loggedUser.getUserHasLanguages()
+                    .stream()
+                    .map(UserHasLanguages::getLanguage)
+                    .collect(Collectors.toList()));
         }
         return allLocales;
     }
