72f5edcd783127ddd12287d4512571221623fd49
Jakub Balhar <jakub@balhar.net>
2016-02-09 19:17:18 +0100

    Dirty fix for bug with loading data about user.

--- src/main/java/cz/larpovadatabaze/lang/DbSessionLanguageSolver.java
+++ src/main/java/cz/larpovadatabaze/lang/DbSessionLanguageSolver.java
@@ -4,6 +4,7 @@ import cz.larpovadatabaze.entities.CsldUser;
 import cz.larpovadatabaze.entities.UserHasLanguages;
 import cz.larpovadatabaze.security.CsldAuthenticatedWebSession;
 import cz.larpovadatabaze.utils.UserUtils;
+import org.hibernate.LazyInitializationException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -24,6 +25,13 @@ public class DbSessionLanguageSolver implements LanguageSolver {
     public List<Locale> getLanguagesForUser() {
         List<Locale> allLocales = new ArrayList<>();
         CsldUser loggedUser = CsldAuthenticatedWebSession.get().getLoggedUser();
+        try {
+            for(UserHasLanguages lang: loggedUser.getUserHasLanguages()){
+                lang.getLanguage();
+            }
+        } catch(LazyInitializationException ex) {
+            loggedUser = UserUtils.getLoggedUser();
+        }
         if(loggedUser != null) {
             allLocales.addAll(loggedUser.getUserHasLanguages().stream()
                     .map(lang -> Locale.forLanguageTag(lang.getLanguage()))
@@ -41,6 +49,13 @@ public class DbSessionLanguageSolver implements LanguageSolver {
     public List<String> getTextLangForUser() {
         List<String> allLocales = new ArrayList<>();
         CsldUser loggedUser = CsldAuthenticatedWebSession.get().getLoggedUser();
+        try {
+            for(UserHasLanguages lang: loggedUser.getUserHasLanguages()){
+                lang.getLanguage();
+            }
+        } catch(LazyInitializationException ex) {
+            loggedUser = UserUtils.getLoggedUser();
+        }
         if(loggedUser != null) {
             allLocales.addAll(loggedUser.getUserHasLanguages()
                     .stream()
