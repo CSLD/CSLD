42b95cb80c202e0d43093d23d6d07eea86000ee1
Jakub Balhar <jakub@balhar.net>
2019-12-16 11:25:38 +0100

    Initial service change.

--- src/main/java/cz/larpovadatabaze/services/FileService.java
+++ src/main/java/cz/larpovadatabaze/services/FileService.java
@@ -35,14 +35,14 @@ public interface FileService {
      *
      * @return File object for the specified name in CSLD's data directory
      */
-    File getPathInDataDir(String relativeName);
+    String getPathInDataDir(String relativeName);
 
     /**
      * @param relativeName Relative name
      *
      * @return File object for preview for the specified name in CSLD's data directory
      */
-    File getFilePreviewInDataDir(String relativeName);
+    String getFilePreviewInDataDir(String relativeName);
 
     /**
      * @param relativeName Relative file name
@@ -55,12 +55,12 @@ public interface FileService {
     /**
      * Respond to resource request with the given file
      *
-     * @param file File to output
+     * @param pathToTheFile Path to the file to the output.
      * @param contentType Content type (if known)
      *
      * @return Resonse that will produce file data
      */
-    AbstractResource.ResourceResponse respondWithFile(File file, String contentType);
+    AbstractResource.ResourceResponse respondWithFile(String pathToTheFile, String contentType);
 
     /**
      * Resize and save uploaded image
