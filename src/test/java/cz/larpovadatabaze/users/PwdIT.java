cc5aa4747db69da70b0e8ccbea0ec38a4f9ae319
Michal Kára <lemming@ucw.cz>
2021-02-01 22:31:09 +0100

    Fix pwd test

--- src/test/java/cz/larpovadatabaze/users/PwdIT.java
+++ src/test/java/cz/larpovadatabaze/users/PwdIT.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class PwdIT {
     @Test
     public void creatingAndVerifyingPassword() {
-        String hash = "1000:616161406262622e637a:ec1473f8cc336460bf9d1dd30f77b84d20f4931fc1b1ca926a183996f78b8cf3e242ed6b655f69abe61a934cdea57d41378808d4cf04108025768ede4f82bf7a"; // Pwd.generateStrongPasswordHash("aaa", "aaa@bbb.cz");
+        String hash = Pwd.generateStrongPasswordHash("aaa", "aaa@bbb.cz");
 
         boolean valid = Pwd.validatePassword("aaa", hash);
         assertEquals(valid, true);
